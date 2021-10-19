e6feccecc02337c8c6406bfaa30b7a4c1964317f
Joselynn Wallace <jwalla12@tdatascicit.services.brown.edu>
2021-10-19 09:36:16 -0400

    adding DESeq2 and IHW to packages r script

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
 install.packages(c("patchwork","tidyverse","ggrepel","knitr","kableExtra","stringr","plotly","viridis","pdftools","openxlsx","data.table","reactable","purrr","htmltools","ggiraph"), repos = "http://cran.rstudio.com/")
 install.packages(c("cowplot","plotly","RColorBrewer"), repos = "http://cran.rstudio.com/")
-BiocManager::install(c("AnnotationHub","biomaRt", "ensembldb", "GenomicRanges", "DT", "ComplexHeatmap", "BiocGenerics", "limma", "S4Vectors", "SummarizedExperiment", "TFBSTools", "JASPAR2018", "GenomeInfoDb", "IRanges", "SummarizedExperiment", "AnnoatationDbi", "clusterProfiler", "DOSE", "enrichplot", "ReactomePA", "bsseq", "Biostrings", "DSS", "methyAnalysis", "ChIPpeakAnno", "ChIPseeker", "methylSig", "DiffBind"))
+BiocManager::install(c("IHW", "DESeq2", "AnnotationHub","biomaRt", "ensembldb", "GenomicRanges", "DT", "ComplexHeatmap", "BiocGenerics", "limma", "S4Vectors", "SummarizedExperiment", "TFBSTools", "JASPAR2018", "GenomeInfoDb", "IRanges", "SummarizedExperiment", "AnnoatationDbi", "clusterProfiler", "DOSE", "enrichplot", "ReactomePA", "bsseq", "Biostrings", "DSS", "methyAnalysis", "ChIPpeakAnno", "ChIPseeker", "methylSig", "DiffBind"))
 
